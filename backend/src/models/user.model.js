526a916d8a8d03e1b04d11e03f370b047e44f7c6
sahilpadmani <sahilpadmani616@gmail.com>
2024-12-18 00:23:23 +0530

    from sahil

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,6 @@
+//hii this is from sahil padmanib
+//hii this is from sahil padmanib
+//hii this is from sahil padmanib
+//hii this is from sahil padmanib
+//hii this is from sahil padmanib
+//hii this is from sahil padmanib
