e40ff66a9e10bf49483327f6847169a1f40866c2
jaymehta2189 <jaymehta249@gmail.com>
2025-03-11 00:30:18 +0530

    deploy fixed error redis

--- backend/src/service/configRedis.js
+++ backend/src/service/configRedis.js
@@ -1,12 +1,6 @@
 const Redis = require('ioredis');
 require('dotenv').config();
-const RedisClient = new Redis({
-  host: process.env.REDIS_URL,
-  port: 6379, 
-  tls: {} ,
-  connectTimeout: 10000
-  // Required for secure connection
-});
+const RedisClient = new Redis(process.env.REDIS_URL,{tls:{}});
 // const pubClient = new Redis(process.env.REDIS_URL);
 // const subClient = pubClient.duplicate();
 
